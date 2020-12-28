135cd30fe4b11dc2f9aad9c175bf442137e8ef4c
Matthias Stein <m.stein@conterra.de>
2020-12-28 11:10:39 +0100

    Improve tests

--- src/main/js/bundles/dn_tocactionrenderer/tests/TocRendererChanger.js
+++ src/main/js/bundles/dn_tocactionrenderer/tests/TocRendererChanger.js
@@ -15,15 +15,15 @@
  */
 import registerSuite from "intern!object";
 import assert from "intern/chai!assert";
-import module from  "module";
+import md from "module";
 import tocRendererChangerFactory from "../tocRendererChangerFactory";
 
 registerSuite({
     name: md.id,
     "expect that 1 added to 1 equals 2": function () {
-        var test = new tocRendererChangerFactory ();
-        // exchange this with a more clever test ;)
-        var result = 1 + 1;
+        const test = new tocRendererChangerFactory ();
+        // TODO: exchange this with a more clever test ;)
+        const result = 1 + 1;
         assert.equal(result, 2);
     }
 });
