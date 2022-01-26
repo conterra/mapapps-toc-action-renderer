273f360459ff9f53b1dd80bf8130ea824d3c73f1
Matthias Stein <m.stein@conterra.de>
2022-01-26 21:56:38 +0100

    Fix linting errors in TocRendererChangerWidget.vue

--- src/main/js/bundles/dn_tocactionrenderer/TocRendererChangerWidget.vue
+++ src/main/js/bundles/dn_tocactionrenderer/TocRendererChangerWidget.vue
@@ -17,14 +17,21 @@
 -->
 <template>
     <v-container pa-0>
-        <v-layout column wrap>
-            <v-flex xs12 mb-2 v-if="selectedLayerId">
+        <v-layout
+            column
+            wrap
+        >
+            <v-flex
+                v-if="selectedLayerId"
+                xs12
+                mb-2
+            >
                 <v-select
                     v-model="selectedRenderer"
                     :items="rendererItems"
                     :label="i18n.renderer"
                     hide-details
-                ></v-select>
+                />
                 <div v-if="selectedLayerId && selectedRenderer !== 'simple' && selectedRenderer !== 'heatmap'">
                     <v-select
                         v-model="selectedAttribute"
@@ -33,7 +40,7 @@
                         item-value="name"
                         :label="i18n.attribute"
                         hide-details
-                    ></v-select>
+                    />
                 </div>
             </v-flex>
             <v-flex x12>
@@ -41,18 +48,24 @@
                     <v-card class="pa-3 secondary">
                         <p>Fill Color Picker:</p>
                         <div class="tocactionrenderer_color_picker">
-                            <color-picker v-model="colorPickerValue"/>
+                            <color-picker v-model="colorPickerValue" />
                         </div>
-                        <div class="mb-4"/>
+                        <div class="mb-4" />
                         <p>Outline Color Picker:</p>
                         <div class="tocactionrenderer_color_picker">
-                            <outline-color-picker v-model="outlineColorPickerValue"/>
+                            <outline-color-picker v-model="outlineColorPickerValue" />
                         </div>
                     </v-card>
                 </div>
             </v-flex>
-            <v-flex xs12 v-if="selectedRenderer">
-                <v-btn class="secondary ml-0" @click="$emit('reset-renderer')">
+            <v-flex
+                v-if="selectedRenderer"
+                xs12
+            >
+                <v-btn
+                    class="secondary ml-0"
+                    @click="$emit('reset-renderer')"
+                >
                     {{ i18n.resetRenderer }}
                 </v-btn>
             </v-flex>
@@ -60,117 +73,117 @@
         <div
             ref="ctSmartRendererWidgets"
             class="tocactionrenderer-esri-widgets"
-        ></div>
+        />
     </v-container>
 </template>
 
 <script>
-/*
-TODO:
-- different color schemes: https://developers.arcgis.com/javascript/latest/api-reference/esri-renderers-smartMapping-symbology-color.html#getSchemes
-- error handling
-- filter renderer for attr types
-- heatmap renderer
-*/
-import Bindable from "apprt-vue/mixins/Bindable";
-import {Slider} from 'dn_vuecolor';
+    /*
+    TODO:
+    - different color schemes: https://developers.arcgis.com/javascript/latest/api-reference/esri-renderers-smartMapping-symbology-color.html#getSchemes
+    - error handling
+    - filter renderer for attr types
+    - heatmap renderer
+    */
+    import Bindable from "apprt-vue/mixins/Bindable";
+    import {Slider} from 'dn_vuecolor';
 
-export default {
-    components: {
-        'color-picker': Slider,
-        'outline-color-picker': Slider
-    },
-    mixins: [Bindable],
-    data: function () {
-        return {
-            name: "",
-            i18n: Object,
-            message: "",
-            selectedLayerAttributes: [],
-            selectedLayerId: undefined,
-            selectedAttribute: undefined,
-            selectedRenderer: "simple",
-            color: [],
-            outlineColor: [],
-            allowedRenderers: []
-        };
-    },
+    export default {
+        components: {
+            'color-picker': Slider,
+            'outline-color-picker': Slider
+        },
+        mixins: [Bindable],
+        data: function () {
+            return {
+                name: "",
+                i18n: Object,
+                message: "",
+                selectedLayerAttributes: [],
+                selectedLayerId: undefined,
+                selectedAttribute: undefined,
+                selectedRenderer: "simple",
+                color: [],
+                outlineColor: [],
+                allowedRenderers: []
+            };
+        },
 
-    computed: {
-        colorPickerValue: {
-            get() {
-                const color = this.color;
-                if (color.length === 4) {
-                    return {
-                        r: color[0],
-                        g: color[1],
-                        b: color[2],
-                        a: color[3]
-                    };
-                } else {
-                    return {
-                        r: 255,
-                        g: 0,
-                        b: 0,
-                        a: 0
-                    };
+        computed: {
+            colorPickerValue: {
+                get() {
+                    const color = this.color;
+                    if (color.length === 4) {
+                        return {
+                            r: color[0],
+                            g: color[1],
+                            b: color[2],
+                            a: color[3]
+                        };
+                    } else {
+                        return {
+                            r: 255,
+                            g: 0,
+                            b: 0,
+                            a: 0
+                        };
+                    }
+                },
+                set(value) {
+                    const rgba = value.rgba;
+                    this.color = [rgba.r, rgba.g, rgba.b, rgba.a];
                 }
             },
-            set(value) {
-                const rgba = value.rgba;
-                this.color = [rgba.r, rgba.g, rgba.b, rgba.a];
-            }
-        },
-        outlineColorPickerValue: {
-            get() {
-                const outlineColor = this.outlineColor;
-                if (outlineColor.length === 4) {
-                    return {
-                        r: outlineColor[0],
-                        g: outlineColor[1],
-                        b: outlineColor[2],
-                        a: outlineColor[3]
-                    };
-                } else {
-                    return {
-                        r: 255,
-                        g: 0,
-                        b: 0,
-                        a: 0
-                    };
+            outlineColorPickerValue: {
+                get() {
+                    const outlineColor = this.outlineColor;
+                    if (outlineColor.length === 4) {
+                        return {
+                            r: outlineColor[0],
+                            g: outlineColor[1],
+                            b: outlineColor[2],
+                            a: outlineColor[3]
+                        };
+                    } else {
+                        return {
+                            r: 255,
+                            g: 0,
+                            b: 0,
+                            a: 0
+                        };
+                    }
+                },
+                set(value) {
+                    const rgba = value.rgba;
+                    this.outlineColor = [rgba.r, rgba.g, rgba.b, rgba.a];
                 }
             },
-            set(value) {
-                const rgba = value.rgba;
-                this.outlineColor = [rgba.r, rgba.g, rgba.b, rgba.a];
+            rendererItems: {
+                get() {
+                    return this.i18n.renderers.filter((renderer) => this.allowedRenderers.includes(renderer.value));
+                }
             }
         },
-        rendererItems: {
-            get() {
-                return this.i18n.renderers.filter((renderer) => this.allowedRenderers.includes(renderer.value));
-            }
-        }
-    },
 
-    watch: {
-        selectedAttribute: function (attr) {
-            if (attr) {
-                this.updateRenderer();
+        watch: {
+            selectedAttribute: function (attr) {
+                if (attr) {
+                    this.updateRenderer();
+                }
+            },
+            selectedRenderer: function (renderer) {
+                if (renderer) {
+                    this.updateRenderer();
+                }
             }
         },
-        selectedRenderer: function (renderer) {
-            if (renderer) {
-                this.updateRenderer();
+        methods: {
+            updateRenderer() {
+                this.$emit("update-renderer", {
+                    attribute: this.selectedAttribute,
+                    renderer: this.selectedRenderer
+                });
             }
         }
-    },
-    methods: {
-        updateRenderer() {
-            this.$emit("update-renderer", {
-                attribute: this.selectedAttribute,
-                renderer: this.selectedRenderer
-            });
-        }
-    }
-};
+    };
 </script>
