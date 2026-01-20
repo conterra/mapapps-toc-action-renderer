7d6b17078ee2ee1ed3cce7a002f0f0791a5e65f4
Pascal Swienty <p.swienty@conterra.de>
2026-01-20 11:26:13 +0100

    rewrite the i1n8 classes to typescript

--- src/main/js/bundles/dn_tocactionrenderer/nls/bundle.ts
+++ src/main/js/bundles/dn_tocactionrenderer/nls/bundle.ts
@@ -1,3 +1,19 @@
+///
+/// Copyright (C) 2025 con terra GmbH (info@conterra.de)
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///         http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+
 /*
  * Copyright (C) 2025 con terra GmbH (info@conterra.de)
  *
@@ -13,7 +29,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export default {
+const i18n = {
     root: ({
         bundleName: "dn_tocactionrenderer",
         bundleDescription: "Widget containing the esri smart renderer functionalities",
@@ -45,3 +61,9 @@ export default {
     }),
     "de": true
 };
+
+export type Messages = (typeof i18n)["root"];
+export interface MessagesReference {
+    get: () => Messages
+}
+export default i18n;
